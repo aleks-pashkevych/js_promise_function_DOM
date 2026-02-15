16ea0b0b5fd086131fa4bd753f549117fea6bb88
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-02-15 21:37:25 +0200

    task with errors, need to check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,24 @@
 
 function waitFor(element, eventName) {
   // write your code here
+  const prom = new Promise((resolve, reject) => {
+    element.addEventListener(eventName, (e) => {
+      if (e.target === element && element.textContent.length > 0) {
+        resolve(element.textContent);
+      }
+    });
+  });
+
+  return prom;
 }
 
 const printMessage = (message) => {
   // write your code here
+  const div = document.createElement('div');
+
+  div.className.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
